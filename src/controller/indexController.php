4705d210b3132de13fd8f474ac5b29b47081dd33
Tubaro Federico Ezequiel <fedetubaro@hotmail.com>
2017-12-18 09:39:20 -0300

    Final

--- src/controller/indexController.php
+++ src/controller/indexController.php
@@ -4,9 +4,22 @@
 class indexController extends Controller {
 
   public function index(){
-
+    if ($_POST) {
+      FlashMsj::getInstance()->setMsj('success',"Se envio correctamente el mensaje");
+    }
 
     $this->_view->render('index.html.twig');
+  }
+
+  public function nosotros(){
+
+    $this->_view->render('nosotros.html.twig');
+  }
+
+  public function contacto(){
+
+
+    $this->_view->render('contacto.html.twig');
   }
 
 
